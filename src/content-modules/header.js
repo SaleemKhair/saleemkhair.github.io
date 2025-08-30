bf02e134e8ca8735bc726a0638b6d72e53e69209
Saleem Khair <sa-lee-m-af11@hotmail.com>
2025-08-30 07:12:08 +0300

    Update header content for job title and focus

--- src/content-modules/header.js
+++ src/content-modules/header.js
@@ -1,6 +1,6 @@
 export const headerContent = `# Saleem Khair
 
-## Software Engineer | FinTech | Payment Systems & Infrastructure
+## Software Engineer | FinTech | Payment Orchestration & Infrastructure | SaaS
 
 [Amman, Jordan](location) [saleemkhair@gmail.com](mailto:saleemkhair@gmail.com) [+962-79-604-9560](tel:+962796049560) [LinkedIn](https://www.linkedin.com/in/saleem-khair-359795108)
 `;
